e9652a79df8394efe01845a30c943428eff617da
DrDesus <shybut69@gmail.com>
2018-10-10 22:43:11 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app=express();
+
+
+app.get('/hello',(req,res)=>{
+    res.send('pisos');
+});
+
+app.post('/hello',(req,res)=>{
+    let arr=[1,2,3];
+    res.send(arr);
+})
+
+app.post('/users/:id',(req,res)=>{
+    let user={
+        name:20,
+        age:15,
+        pisos:16
+    }
+    const { params } = req;
+    console.log(`id:${params.id}`);
+    if(params.id==12){
+        res.send(user);
+    }
+    else res.send('con')
+})
+
+app.listen(8080,()=>{
+    console.log('server started');
+});
